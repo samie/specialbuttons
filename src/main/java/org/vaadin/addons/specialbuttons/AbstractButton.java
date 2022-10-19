2dc33f127ca06d5349b4e5c19d9179d532c0a5ff
samie <sami@vaadin.com>
2022-10-19 09:17:48 +0300

    Use clicknotifier mixin

--- src/main/java/org/vaadin/addons/specialbuttons/AbstractButton.java
+++ src/main/java/org/vaadin/addons/specialbuttons/AbstractButton.java
@@ -1,9 +1,8 @@
 package org.vaadin.addons.specialbuttons;
 
 import com.vaadin.flow.component.*;
-import com.vaadin.flow.shared.Registration;
 
-public abstract  class AbstractButton extends Component implements HasStyle, HasSize{
+public abstract  class AbstractButton extends Component implements HasStyle, HasSize, ClickNotifier {
 
 
     private boolean enabled;
@@ -51,17 +50,5 @@ public abstract  class AbstractButton extends Component implements HasStyle, Has
         if (!isEnabled()) return;
         super.fireEvent(clickEvent);
     }
-    public Registration addClickListener(
-            ComponentEventListener<ClickEvent> listener) {
-        if (this instanceof Component) {
-            return ComponentUtil.addListener((Component) this, ClickEvent.class,
-                    (ComponentEventListener) listener);
-        } else {
-            throw new IllegalStateException(String.format(
-                    "The class '%s' doesn't extend '%s'. "
-                            + "Make your implementation for the method '%s'.",
-                    getClass().getName(), Component.class.getSimpleName(),
-                    "addClickListener"));
-        }
-    }
+
 }
